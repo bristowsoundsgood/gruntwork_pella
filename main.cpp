2f14a07f7bcdb5526228811432ccd1576aef141e
bristowsoundsgood <bristowsoundsgood@gmail.com>
2025-07-10 23:10:28 +0100

    First discogs API call test. URL works, but the curl call does not. Fix inbound

--- main.cpp
+++ main.cpp
@@ -1,23 +1,54 @@
 // Main.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+
+// Send as header, not as query
+// JSON -> Object needed
+
+/*
+	Database search query endpoint:
+	https://api.discogs.com/database/search?q={query}&{?type,title,release_title,credit,artist,anv,label,genre,style,country,year,format,catno,barcode,track,submitter,contributor}
+*/
 
 #include <iostream>
 #include <curl/curl.h>
 #include <string>
+#include <string_view>
 
 size_t write_callback(void *ptr, size_t size, size_t count, void *stream) {
 	((std::string*) stream)->append((char*)ptr, 0, size*count);
 	return size*count;
 }
 
+std::string get_environment_variable(const std::string_view& varname) {
+	char* buf = nullptr;
+	size_t sz = 0;
+
+	if (_dupenv_s(&buf, &sz, varname.data()) != 0 || buf == nullptr) {
+		throw std::runtime_error(std::format("Failed to get environment variable: {}", varname));
+	}
+	std::string result(buf, sz);
+
+	free(buf);
+	return result;
+}
+
 int main () {
 
-	CURL* curl = curl_easy_init ();		// Handles a single network request
-	const char* endpoint = "https://meowfacts.herokuapp.com/";
+	CURL* curl = curl_easy_init ();		// Handles a network request
+
+	// Discogs API search requests need authentication (env variables for privacy...)
+	std::string consumer_key = get_environment_variable ("DISCOGS_CONSUMER_KEY");
+	std::string consumer_secret = get_environment_variable ("DISCOGS_CONSUMER_SECRET");
+	
+	std::string endpoint = "https://api.discogs.com/database/search?q=acapella";
+	std::string auth_header = "&key=" + consumer_key + "&secret=" + consumer_secret;
+
+
+	std::string full_url = endpoint + auth_header;		// Must concatenate before passing into the next line (avoids dangling pointers caused by temporary object)
+	const char* url = full_url.c_str ();				// curl is a C library, therefore std::strings cannot be passed
 
 	if (curl) {
-		curl_easy_setopt (curl, CURLOPT_URL, endpoint);		// Set URL to work with		
-
+		curl_easy_setopt (curl, CURLOPT_URL, url);		// Set URL to work with		
 		
 		std::string response{};
 		curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, write_callback);		// Callback function is invoked as soon as data are received. Called many times until all packets have arrived.
@@ -36,7 +67,6 @@ int main () {
 
 		curl_easy_cleanup (curl);
 	}
-
 
 	return 0;
 }

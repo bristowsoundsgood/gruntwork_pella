68da75773bf374ae7c95477b63d484f194298395
bristowsoundsgood <bristowsoundsgood@gmail.com>
2025-07-13 00:19:32 +0100

    Implemented successful Discogs search request. Required user-agent and authorisation headers.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <curl/curl.h>
 #include <string>
 #include <string_view>
+#include <format>
 
 size_t write_callback(void *ptr, size_t size, size_t count, void *stream) {
 	((std::string*) stream)->append((char*)ptr, 0, size*count);
@@ -26,7 +27,7 @@ std::string get_environment_variable(const std::string_view& varname) {
 	if (_dupenv_s(&buf, &sz, varname.data()) != 0 || buf == nullptr) {
 		throw std::runtime_error(std::format("Failed to get environment variable: {}", varname));
 	}
-	std::string result(buf, sz);
+	std::string result(buf);
 
 	free(buf);
 	return result;
@@ -34,27 +35,36 @@ std::string get_environment_variable(const std::string_view& varname) {
 
 int main () {
 
-	CURL* curl = curl_easy_init ();		// Handles a network request
+	CURL* curl = curl_easy_init ();											// Handles a network request
+	struct curl_slist* header_list = NULL;									// List of request headers to send
 
+	std::string endpoint = "https://api.discogs.com/database/search?q=acapella";
+	
 	// Discogs API search requests need authentication (env variables for privacy...)
 	std::string consumer_key = get_environment_variable ("DISCOGS_CONSUMER_KEY");
-	std::string consumer_secret = get_environment_variable ("DISCOGS_CONSUMER_SECRET");
-	
-	std::string endpoint = "https://api.discogs.com/database/search?q=acapella";
-	std::string auth_header = "&key=" + consumer_key + "&secret=" + consumer_secret;
+	std::string consumer_secret = get_environment_variable ("DISCOGS_CONSUMER_SECRET");	
 
-
-	std::string full_url = endpoint + auth_header;		// Must concatenate before passing into the next line (avoids dangling pointers caused by temporary object)
-	const char* url = full_url.c_str ();				// curl is a C library, therefore std::strings cannot be passed
+	std::string header_auth = "Authorization: Discogs key=" + consumer_key + ", secret=" + consumer_secret;
+	std::string header_user_agent = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0";		
+																			// User Agent header is needed for access (else will be forbidden)	
 
 	if (curl) {
-		curl_easy_setopt (curl, CURLOPT_URL, url);		// Set URL to work with		
-		
+
+		// Build the network request (url, header, useragent). c_str() is used because std::strings cannot be passed (curl is a C library)	
+		curl_easy_setopt (curl, CURLOPT_URL, endpoint.c_str());											
+		header_list = curl_slist_append (header_list, header_auth.c_str());
+		curl_easy_setopt (curl, CURLOPT_HTTPHEADER, header_list);		
+		curl_easy_setopt (curl, CURLOPT_USERAGENT, header_user_agent.c_str());
+
+		std::cout << '\n' << "URL: " << endpoint.c_str() << '\n';
+		std::cout << "Authorization Header: " << header_auth.c_str() << '\n';
+		std::cout << "User Agent Header: " << header_user_agent.c_str() << '\n';
+
+		// Get response and save
 		std::string response{};
 		curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, write_callback);		// Callback function is invoked as soon as data are received. Called many times until all packets have arrived.
 		curl_easy_setopt (curl, CURLOPT_WRITEDATA, &response);				// Write data to response object
 		curl_easy_perform (curl);											// Perform network request (blocking manner, waits for transfer to complete/fail)
-
 		
 		if (!response.empty()) {
 			std::cout << '\n' << "RETURNED:" << '\n';
@@ -66,6 +76,7 @@ int main () {
 		}
 
 		curl_easy_cleanup (curl);
+		curl_slist_free_all (header_list);
 	}
 
 	return 0;
